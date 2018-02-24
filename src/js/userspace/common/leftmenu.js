8fb51d4921f7557772da86752e4e3afe0a9d4088
Pytonfo <pythonfo@gmail.com>
2018-02-24 18:16:19 +0800

    包裹进iife

--- src/js/userspace/common/leftmenu.js
+++ src/js/userspace/common/leftmenu.js
@@ -1,25 +1,21 @@
-(function () {
-    userSpaceLeftMenu();
-})();
-
-function userSpaceLeftMenu() {
-    $('#left-menu').load('/public/leftmenu.html', function () {
-        $('#sendpost-leftmenu')[0].flag = true;
-        $('#sendpost-leftmenu').on('click.post', function () {
-            if ($(this)[0].flag) {
-                $(this)[0].flag = false;
-                var dataUrl = 'http://userspace1.macaoeasybuy.com/UserPublishController/queryUserPublishCount.easy?userId=' + userId + '&easybuyCallback=?';
-                $.getJSON(dataUrl, function (data) {
-                    var newData = data.publishCount;
-                    $.each(newData, function (k, y) {
-                        $('#' + k + '-postclick p span').html(y);
-                    });
+void function() {
+  $('#left-menu').load('/public/leftmenu.html', function () {
+    $('#sendpost-leftmenu')[0].flag = true;
+    $('#sendpost-leftmenu').on('click.post', function () {
+        if ($(this)[0].flag) {
+            $(this)[0].flag = false;
+            var dataUrl = 'http://userspace1.macaoeasybuy.com/UserPublishController/queryUserPublishCount.easy?userId=' + userId + '&easybuyCallback=?';
+            $.getJSON(dataUrl, function (data) {
+                var newData = data.publishCount;
+                $.each(newData, function (k, y) {
+                    $('#' + k + '-postclick p span').html(y);
                 });
-            }
-            $('#sendpost-leftmenu-box').css('display', 'block');
-        });
-        $('#sendpost-leftmenu-box img.cancel-btn').on('click', function () {
-            $('#sendpost-leftmenu-box').css('display', 'none');
-        });
+            });
+        }
+        $('#sendpost-leftmenu-box').css('display', 'block');
     });
-}
+    $('#sendpost-leftmenu-box img.cancel-btn').on('click', function () {
+        $('#sendpost-leftmenu-box').css('display', 'none');
+    });
+});
+}();
