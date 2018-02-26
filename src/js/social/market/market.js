88dc4f6e4af32919b01ef77ac2415cab80b3c9ce
lihui <576436588@qq.com>
2018-02-26 17:12:33 +0800

    update liveshot.html

--- src/js/social/market/market.js
+++ src/js/social/market/market.js
@@ -37,45 +37,67 @@ function goFpDetail(a,b){
 	
 }
 function hitScroll(){
-	
-}
-//檢測是否到達視區
-function lazyLoad(){
 	//存儲偏移量
 	let slidemap = {};
-	//每一個分類框
-	let aLi = document.getElementsByClassName('marketBlock_main_each');
-	console.log(aLi);
-	let t = document.documentElement.clientHeight + (document.documentElement.scrollTop || document.body.scrollTop)-300;
-	console.log(t);
-	for(let i = 0; i<aLi.length; i++){
-		let oLi = aLi[i];
-		let h = getH(oLi);
-		if (h < t) {
-			setImg(i);
+	/*throttling節流*/
+	function throttling(fn, wait, maxTimelong) {
+	    var timeout = null,
+	        startTime = Date.parse(new Date);
+	
+	    return function() {
+	        if(timeout !== null) clearTimeout(timeout);
+	        var curTime = Date.parse(new Date);
+	        if(curTime-startTime>=maxTimelong) {
+	            fn();
+	            startTime = curTime;
+	        } else {
+	            timeout = setTimeout(fn, wait);
+	        }
+	    }
+	}
+	/*執行函數*/
+	function handle() {
+		//填入要執行的函數
+		   lazyLoad();
+	 }
+	//檢測是否到達視區
+	function lazyLoad(){
+		
+		
+		//每一個分類框
+		let aLi = document.getElementsByClassName('marketBlock_main_each');
+		let t = document.documentElement.clientHeight + (document.documentElement.scrollTop || document.body.scrollTop)-300;
+		for(let i = 0; i<aLi.length; i++){
+			let oLi = aLi[i];
+			let h = getH(oLi);
+			if (h < t) {
+				setImg(i);
+			}
 		}
 	}
-}
-function setImg(index){
-	let aLiImg = $('.market_main .marketBlock_main_each.noPic').children('img');
-	let src = $(aLiImg[index]).attr('src');
-	$(aLiImg[index]).attr('src',src);
-	$(aLiImg[index]).css('opacity','1');
-	$(aLiImg[index]).parents('.marketBlock_main_each').removeClass('noPic');
-}
-function getH(obj){
-	let id=$(obj).data('id');
-	let h=0;
-	if(slidemap[id]){
-		h = slidemap[id].height;
-	}else{
-		while(obj){
-			h += obj.offsetTop;
-			obj = obj.offsetParent;
-		}
-		slidemap[id] = {
-			height:h
-		}
+	function setImg(index){
+		let aLiImg = $('.market_main .marketBlock_main_each.noPic').children('img');
+		let src = $(aLiImg[index]).attr('src');
+		$(aLiImg[index]).attr('src',src);
+		$(aLiImg[index]).css('opacity','1');
+		$(aLiImg[index]).parents('.marketBlock_main_each').removeClass('noPic');
 	}
-	return h;
+	function getH(obj){
+		let id=$(obj).data('id');
+		let h=0;
+		if(slidemap[id]){
+			h = slidemap[id].height;
+		}else{
+			while(obj){
+				h += obj.offsetTop;
+				obj = obj.offsetParent;
+			}
+			slidemap[id] = {
+				height:h
+			}
+		}
+		return h;
+	}
+	/*綁定滾動*/
+	$(window).off('scroll').on('scroll',throttling(handle, 300, 1000))
 }
